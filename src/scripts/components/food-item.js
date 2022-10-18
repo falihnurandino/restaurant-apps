323884c1c6f281a0c436e37132b0fc06aaca4399
falihnurandino <andinofalih1@gmail.com>
2022-10-18 13:53:35 +0700

    create food-item

---
+++ src/scripts/components/food-item.js
@@ -0,0 +1,23 @@
+class FoodItem extends HTMLElement {
+  set food (food) {
+    this._food = food
+    this.render()
+  }
+
+  get food () {
+    return this._food
+  }
+
+  render () {
+    this.innerHTML = `
+    <a href="#">
+      <section>
+        <img data-src="${this._food.image}" src="/icons/placeholder.png" alt="${this._food.title}" class="food-image"/>
+        <h4 class="card__title text-overflow">${this._food.title}</h4>
+      </section>
+    </a>
+    `
+  }
+}
+
+export default FoodItem

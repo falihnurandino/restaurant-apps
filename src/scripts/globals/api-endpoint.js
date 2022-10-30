9ad178ee389e1aa5087c33924cfb4f8ea9fe893e
falihnurandino <andinofalih1@gmail.com>
2022-10-30 14:11:29 +0700

    add api endpoint meal

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,7 +3,8 @@ import CONFIG from './config'
 const API_ENDPOINT = {
   HOME: `${CONFIG.BASE_URL}list`,
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
-  REVIEW: `${CONFIG.BASE_URL}review`
+  REVIEW: `${CONFIG.BASE_URL}review`,
+  MEAL: CONFIG.BASE_URL_MEAL
 }
 
 export default API_ENDPOINT

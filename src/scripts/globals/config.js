5b18561c9a713bd5224b9a41af6c0aacd47fdad1
falihnurandino <andinofalih1@gmail.com>
2022-11-03 11:36:20 +0700

    add config base url meal

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_URL_MEAL: 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian',
   DATABASE_NAME: 'resto-db',
   DATABASE_VERSION: 1,
   CACHE_NAME: 'resto-cache',
